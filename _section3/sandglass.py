90b12084554d679d469fa0e27653f8798a0a52b1
somsomdah <somdah98@gmail.com>
2020-08-12 18:02:42 +0900

    sandglass

--- _section3/sandglass.py
+++ _section3/sandglass.py
@@ -7,23 +7,28 @@ m=int(sys.stdin.readline())
 for _ in range(m):
      r,d,i=map(int,sys.stdin.readline().split())
      if d==1: # 오른쪽으로 회전
-          temp=grid[r-1][i%n:]+grid[r-1][:i%n]
+          #temp=grid[r-1][i%n:]+grid[r-1][:i%n]
+          for _ in range(i):
+               grid[r-1].insert(0,grid[r-1].pop())
      if d==0: # 왼쪽으로 회전
-          temp=grid[r-1][(n-i)%n:]+grid[r-1][:(n-i)%n]
+          #temp=grid[r-1][(n-i)%n:]+grid[r-1][:(n-i)%n]
+          for _ in range(i):
+               grid[r-1].append(grid[r-1].pop(0))
+     #grid[r-1]=temp
 
-     grid[r-1]=temp
 
-
-s,e=1,n
+s,e=0,n
 res=0
 for r in range(n):
-     print(s,e)
-     if s<e
-          res+=sum(grid[r][s:e])
-     if s>e
-          res+=sum(grid[r][e:s])
-
-     s+=1
-     e-=1
+     res+=sum(grid[r][s:e])
+     #print(e,s)
+     if r<n//2:
+          e-=1
+          s+=1
+     if r>=n//2:
+          e+=1
+          s-=1
+          
+          
 
 print(res)
